d338bff47bb72de49fd9330ffcbaa86e096a3fbe
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-05-15 15:47:45 +0200

    aggiunto lo script backup_home.sh

---
+++ backup_home.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Percorso da backuppare
+HOME_DIR="/home/ilda"
+
+# Cartella base backup
+BACKUP_BASE="/opt/backup"
+
+# Numero settimana ISO (01-53)
+WEEK=$(date +%V)
+
+# Data corrente (YYYY-MM-DD)
+DATE=$(date +%Y-%m-%d)
+
+# Cartella backup settimanale
+WEEK_DIR="$BACKUP_BASE/backup_della_settimana_$WEEK"
+
+# Crea la cartella settimanale se non esiste
+mkdir -p "$WEEK_DIR"
+
+# Nome file di backup
+BACKUP_FILE="$WEEK_DIR/backup_del_giorno_$DATE.tar.gz"
+
+# Crea l’archivio compresso
+tar -czf "$BACKUP_FILE" "$HOME_DIR"
